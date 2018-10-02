db7e136b79f0fe299b79a5b64b9706fdd58c1e11
Alican Çubukçuoğlu <alicancubukcuoglu@gmail.com>
2018-10-01 18:59:26 -0700

    Setup routing

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,27 @@
 // @flow
 
 import * as React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import HomePage from '../HomePage';
+import LoginPage from '../LoginPage';
 
 export default function App() {
-  return <div>Hello World</div>;
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+        <hr />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/login" component={LoginPage} />
+      </div>
+    </Router>
+  );
 }
---
+++ src/components/LoginPage/index.js
@@ -0,0 +1,7 @@
+// @flow
+
+import * as React from 'react';
+
+export default function LoginPage() {
+  return <div>LoginPage</div>;
+}
