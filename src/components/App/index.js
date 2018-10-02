164206cade527d8e716d0ef50667b25be2e1008b
Alican Çubukçuoğlu <alicancubukcuoglu@gmail.com>
2018-10-01 18:59:01 -0700

    Cleanup project

---
+++ src/components/App/index.js
@@ -0,0 +1,7 @@
+// @flow
+
+import * as React from 'react';
+
+export default function App() {
+  return <div>Hello World</div>;
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
+// @flow
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const reactRoot = document.getElementById('root');
+if (!reactRoot) {
+  throw new Error('React root element not found');
+}
+
+ReactDOM.render(<App />, reactRoot);
