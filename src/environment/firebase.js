f9bd10816f9aac4285a653f6ed2e9f7d2af7df3f
Alican Çubukçuoğlu <alicancubukcuoglu@gmail.com>
2018-10-03 12:42:30 -0700

    Setup Firebase

---
+++ src/environment/firebase.js
@@ -0,0 +1,14 @@
+// @flow
+
+import firebase from 'firebase/app';
+
+// Initialize Firebase
+const config = {
+  apiKey: 'AIzaSyD89Ki-nurSLbXE4ibacyjGOPOLzHmtD4E',
+  authDomain: 'alicanc-superstargazer.firebaseapp.com',
+  databaseURL: 'https://alicanc-superstargazer.firebaseio.com',
+  projectId: 'alicanc-superstargazer',
+  storageBucket: 'alicanc-superstargazer.appspot.com',
+  messagingSenderId: '170681616875',
+};
+firebase.initializeApp(config);
